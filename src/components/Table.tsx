a50513c50acba327e5692389346bbec2b1ba1b95
Ketlyn Santos <ketlyncastro2004@gmail.com>
2022-05-30 22:39:49 -0300

    Adding actions column

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,7 @@
+import styles from '../styles/Table.module.css'
 import Client from "../core/Client"
+import { IconEdition, IconTrash } from './Icons'
+
 
 interface TableProps {
     clients: Client[]
@@ -13,6 +16,7 @@ export default function Table(props: TableProps) {
                 <th>Nome</th>
                 <th>Idade</th>
                 <th>Email</th>
+                <th>Ações</th>
             </tr>
         )
     }
@@ -20,20 +24,30 @@ export default function Table(props: TableProps) {
     function renderData() {
         return props.clients?.map((client, i) => {
             return (
-                <tr key={client.id}>
+                <tr key={client.id} style={{backgroundColor: `${i % 2 === 0 ? 'gray' : 'white'}`}}>
                     <td>{ client.id }</td>
                     <td>{ client.name }</td>
                     <td>{ client.age }</td>
                     <td>{ client.email }</td>
+                    {renderActions(client)}
                 </tr>
             )
         })
     }
 
+    function renderActions(client: Client) {
+        return (
+            <td style={{display: 'flex'}}>
+                <button className={styles.btnEdition}>{ IconEdition }</button>
+                <button className={styles.iconTrash}>{ IconTrash }</button>
+            </td>
+        )
+    }
+
     return(
-        <table>
+        <table className={styles.table}>
             <thead>{ renderHeader() }</thead>
             <tbody>{ renderData() }</tbody>
         </table>
-    )
+    ) 
 }
